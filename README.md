57277251222a3a34398ecaa58fdad38a2959ebec
lycloudqwq <49870936+lycloudqwq@users.noreply.github.com>
2025-04-07 00:23:29 +0900

    init

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
-# Row.js
-Personal use script written in vanilla JavaScript only
+## Row.js_local
+Only for Keylol
 
-## Function
-- [x] Fetch API
-- [x] Gist Read/Write
-- [x] remoteContent json
-- [x] querySelector
-- [x] addEventListener
-
-## Progress
-- [x] Prototype
-- [x] Code cleanup
-- [x] Site adaptation
-- [x] Add function
-- [x] Release
+### Function
+- [ ] localStorage
+- [ ] Blacklist
--- Row.js
+++ Row.js
@@ -1,81 +1,14 @@
 // ==UserScript==
-// @name         Row.js
-// @version      0.96
-// @author       lycloud
-// @match        https://keylol.com/f*
-// @match        https://www.chiphell.com/forum.php?*
-// @run-at       document-end
-// @description  Personal use script to mark rows as read
+// @name        Row.js_local
+// @namespace   https://github.com/lycloudqwq/Row.js
+// @version     0.96
+// @author      lycloud
+// @include     https://keylol.com/f*
+// @include     https://www.chiphell.com/forum.php?*
+// @run-at      document-end
 // ==/UserScript==
 
-(async () => {
-    const PA_TOKEN = "github_pat_******"
-    const GIST_ID = "******"
-    const GIST_FILE = "rows.json"
-    const SITE = window.location.hostname
-    const SITE_SELECTOR = {
-        "keylol.com": ".xst",
-        "www.chiphell.com": ".s",
-    }
+(() => {
+    const USER_HIDE = []
 
-    let row = document.querySelectorAll(SITE_SELECTOR[SITE])
-    const color = (index) => {
-        for (const element of row[index].closest("tr").children) {
-            element.style.background = "#98D98E"
-        }
-        // PT sites only to color the outer tr
-        if (document.querySelector(".torrents")) {
-            for (const element of document.querySelectorAll(".torrents>tbody>tr")[index + 1].children) {
-                element.style.background = "#98D98E"
-            }
-        }
-        // Disable alternating row colors in dmhy
-        if (SITE === "share.dmhy.org") {
-            for (const element of document.querySelectorAll(".tablesorter tr.even")) {
-                element.setAttribute("class", "odd")
-            }
-        }
-    }
-
-    // Gist Read
-    let remoteContent = await fetch(`https://api.github.com/gists/${GIST_ID}/commits?per_page=1`, {
-        cache: "no-store",
-        headers: { "Authorization": "token " + PA_TOKEN }
-    })
-        .then(response => response.json())
-        .then(data => {
-            return fetch(`https://gist.githubusercontent.com/lycloudqwq/${GIST_ID}/raw/${data[0].version}`)
-                .then(response => response.json())
-                .then(remoteContent => { return remoteContent })
-        })
-
-    for (let index = 0; index < row.length; index++) {
-        let rowId = row[index].href.match(/\d+/)[0]
-        if (remoteContent[SITE].includes(rowId)) {
-            // Mark read rows in Gist storage
-            color(index)
-        } else {
-            // addEventListener for unread rows
-            row[index].addEventListener("mousedown", () => {
-                color(index)
-                // Control the size of remoteContent
-                remoteContent[SITE].push(rowId)
-                if (remoteContent[SITE].length > 1000) {
-                    remoteContent[SITE].splice(0, 400)
-                }
-                // Gist Write
-                fetch(`https://api.github.com/gists/${GIST_ID}`, {
-                    method: "PATCH",
-                    headers: { "Authorization": "token " + PA_TOKEN },
-                    body: JSON.stringify({
-                        files: {
-                            [GIST_FILE]: {
-                                content: JSON.stringify(remoteContent)
-                            }
-                        }
-                    })
-                })
-            }, { once: true })
-        }
-    }
 })();
